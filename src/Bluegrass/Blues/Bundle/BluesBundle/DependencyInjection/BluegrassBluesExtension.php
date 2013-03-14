2c6eac02b4eeb36b4a19362f1cd34a755c40d1a6
gcaseres <gcaseres@gmail.com>
2013-03-14 17:13:05 -0300

    FIX: Ahora BluesBundle configura a AsseticBundle automáticamente para definir los paquetes de assets que necesita.

--- src/Bluegrass/Blues/Bundle/BluesBundle/DependencyInjection/BluegrassBluesExtension.php
+++ src/Bluegrass/Blues/Bundle/BluesBundle/DependencyInjection/BluegrassBluesExtension.php
@@ -6,13 +6,14 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 
 /**
  * This is the class that loads and manages your bundle configuration
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class BluegrassBluesExtension extends Extension
+class BluegrassBluesExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritDoc}
@@ -25,4 +26,34 @@ class BluegrassBluesExtension extends Extension
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
     }
+    
+    public function prepend(ContainerBuilder $container)
+    {
+        /*
+         * Dado que Blues depende de Assetic, se hace un prepend a la configuración
+         * de assetic de los paquetes de assets que utiliza blues.
+         */
+        $config = array(
+            'assets' => array(
+                'bluegrassbluesbundle_stylesheets' => array(
+                    'inputs' => array(
+                        '@BluegrassBluesBundle/Resources/public/css/ViewState/viewState.css'
+                        )
+                    ),
+                'bluegrassbluesbundle_javascripts' => array(
+                    'inputs' => array(
+                        '@BluegrassBluesBundle/Resources/public/js/ViewState/ViewState.js',
+                        )
+                    )                
+                )
+            );
+        
+        foreach ($container->getExtensions() as $name => $extension) {
+            switch ($name) {
+                case 'assetic':
+                    $container->prependExtensionConfig($name, $config);
+                    break;
+            }
+        }
+    }       
 }
