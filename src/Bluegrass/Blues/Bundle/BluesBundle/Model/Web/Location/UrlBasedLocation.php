46539cd4032f9e4a970a87fcc8cc1e4335b4a891
ldelia <lisandro.delia@gmail.com>
2013-03-22 16:47:00 -0300

    ADD Url parsing

--- src/Bluegrass/Blues/Bundle/BluesBundle/Model/Web/Location/UrlBasedLocation.php
+++ src/Bluegrass/Blues/Bundle/BluesBundle/Model/Web/Location/UrlBasedLocation.php
@@ -8,14 +8,15 @@ namespace Bluegrass\Blues\Bundle\BluesBundle\Model\Web\Location;
  */
 class UrlBasedLocation extends WebLocation
 {
+
     private $url;
 
-    public function __construct( $url, $parameters = array() )
-    {
+    public function __construct($url, $parameters = array())
+    {                        
         $this->setUrl($url);
-        $this->setParameters($parameters);
+        $this->setParameters( array_merge( $this->getQueryStringArray($url) , $parameters   ) );
     }
-   
+
     public function getUrl()
     {
         return $this->url;
@@ -23,13 +24,39 @@ class UrlBasedLocation extends WebLocation
 
     public function setUrl($url)
     {
-        $this->url = $url;
+        $this->url = $this->getPath( $url );
     }
 
-    public function generateUrlWith(UrlGeneratorInterface $urlGenerator, $referenceType) {
+    public function generateUrlWith(UrlGeneratorInterface $urlGenerator, $referenceType)
+    {
         return $urlGenerator->generateFromUrlBasedLocation($this);
     }
 
-   
+    protected function getPath( $url )
+    {            
+        return substr( $url, 0, strpos( $url, '?' ) );
+    }
+
+    protected function getQueryStringArray($url)
+    {
+        $result = array();
+        
+        //string must contain at least one = and cannot be in first position
+        if (strpos($url, '=')) {
+            if (strpos($url, '?') !== false) {
+                $q = parse_url($url);
+                $qry = $q['query'];
+            }
+        } else {
+            return array();
+        }
+
+        foreach (explode('&', $qry) as $couple) {
+            list ($key, $val) = explode('=', $couple);
+            $result[$key] = $val;
+        }
+        return empty($result) ? array() : $result;
+    }
+
 }
 
